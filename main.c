b4f9be4f1842ad6763ba6774e57752644a115f4c
LoivalF <flunavio777@gmail.com>
2025-04-11 22:02:44 -0400

    Update main.c

--- main.c
+++ main.c
@@ -12,6 +12,10 @@ typedef struct {
     char horaRegistro[9]; // formato HH:MM:SS
 } Ticket;
 
+/* Esta funcion reserva memoria para 1 ticket y le copia
+la ID y la descripcion entregadas, ademas de eso usa la 
+hora actual del sistema como hora de registro en el 
+formato HH:MM:SS*/
 Ticket* crearTicket(int id, char* descripcion) {
     Ticket* ticket = malloc(sizeof(Ticket));
     if (ticket == NULL) exit(EXIT_FAILURE);
@@ -28,11 +32,18 @@ Ticket* crearTicket(int id, char* descripcion) {
     return ticket;
 }
 
+/*Usa la funcion crear ticket y lo inserta de manera
+predeterminada en la lista de prioridad baja con un
+push back*/
 void registrarTicket(List* baja, int id, char* descripcion) {
     Ticket* nuevo = crearTicket(id, descripcion);
     pushBack(baja, nuevo);
 }
 
+/*Hago una variable de prioridad que se basa en el numero
+que esta guardado en el ticket, 1 para baja, 2 para media 
+y 3 para alta, luego muestro los datos ya guardados en el 
+ticket mas la prioridad*/
 void imprimirTicket(Ticket* ticket) {
     char prioridad[10];
     switch (ticket->prioridad) {
@@ -47,6 +58,9 @@ void imprimirTicket(Ticket* ticket) {
     printf("Hora de registro: %s\n\n", ticket->horaRegistro);
 }
 
+/*Recibe una lista y un ID para buscar, busca 
+desde el primero hasta el ultimo de la lista, 
+si lo encuentra lo retorna, si no retorna nulo*/
 Ticket* buscarTicketPorID(List* lista, int id) {
     Ticket* ticket = first(lista);
     while (ticket != NULL) {
@@ -56,6 +70,9 @@ Ticket* buscarTicketPorID(List* lista, int id) {
     return NULL;
 }
 
+/*Esta funcion hace lo mismo que la de arriba con la 
+diferencia que esta ademas imprime los detalles si es 
+que existe, si no existe muestra un respectivo aviso*/
 Ticket* buscarTicketPorIDyDetalles(List* lista, int id) {
     Ticket* ticket = buscarTicketPorID(lista, id);
     if (ticket) {
@@ -66,6 +83,8 @@ Ticket* buscarTicketPorIDyDetalles(List* lista, int id) {
     return NULL;
 }
 
+/*Funciona de manera parecida a buscar por ID, pero 
+si en esta si es que lo encuentra lo borra y retorna*/
 void eliminarTicketPorID(List* lista, int id) {
     Ticket* ticket = first(lista);
     while (ticket != NULL) {
@@ -77,6 +96,11 @@ void eliminarTicketPorID(List* lista, int id) {
     }
 }
 
+/*Asigna prioridad recibiendo las listas para las 3 prioridades, el ID 
+y la nueva prioridad, va consecutivamente buscando el ID en las lista 
+en orden ascendente, si no lo encuentra lo especifica y en cualquier otro
+caso lo borra desde donde esta, le cambia la prioridad y le hace un 
+push back basado en la nueva prioridad*/
 void asignarPrioridad(List* baja, List* medio, List* alto, int id, unsigned short nuevaPrioridad) {
     Ticket* ticket = buscarTicketPorID(baja, id);
     List* origen = baja;
@@ -110,6 +134,9 @@ void asignarPrioridad(List* baja, List* medio, List* alto, int id, unsigned shor
     }
 }
 
+/*Muestra todos los tickets pendientes entre las 3 listas, si no hay ninguno
+en las 3, lo especifica, en otros casos muestra los tickets de las listas 
+con tamanyo mayor a 0, para imprimirlos va recorriendo cada lista*/
 void mostrarTicketsPendientes(List* baja, List* media, List* alta) {
     if (get_size(alta) == 0 && get_size(media) == 0 && get_size(baja) == 0) {
         printf("No hay tickets pendientes.\n");
@@ -142,7 +169,9 @@ void mostrarTicketsPendientes(List* baja, List* media, List* alta) {
         }
     } 
 }
-
+/*Procesa el siguiente ticket, si no hay tickets en ninguna lista
+lo especifica y si no borra solo 1 ticket de la lista que contenga 
+tickets con la mayor prioridad*/
 void procesarSgteTicket(List* baja, List* media, List* alta) {
     int sizeAlta = get_size(alta);
     int sizeMedia = get_size(media);
@@ -171,6 +200,16 @@ void procesarSgteTicket(List* baja, List* media, List* alta) {
     }
 }
 
+/*Crea 1 lista para cada prioridad y asigna las variables a usar
+hace un do while para que el programa se ejecute hasta que el 
+usuario lo requiera, se muestra el menu y la ultima opcion finaliza
+el programa, limpia la pantalla en cada opcion, en la primera, 
+ingresar un nuevo ticket, se revisa si el ID ya existe, en la 
+segunda, cambiar prioridad, se revisa si el ticket no existe,
+igual que con la prioridad, que se repite hasta que este entre
+1 y 3, opcion 3 y 4 funcionan normalmente y en la 5, buscar
+ticket por ID se revisa que el ID sea valido y la opcion 6
+es para salir, liberando todas las listas antes*/
 int main() {
     List* listaBaja = create_list();
     List* listaMedia = create_list();
@@ -214,12 +253,12 @@ int main() {
             case 2:
                 system("cls||clear");
                 printf("Ingrese el ID del ticket a modificar: ");
-                do {
-                    scanf("%d", &id);
-                    if (buscarTicketPorID(listaBaja, id) == NULL && buscarTicketPorID(listaMedia, id) == NULL && buscarTicketPorID(listaAlta, id) == NULL) {
-                        printf("El ID no existe. Ingrese otro ID: ");
-                    }
-                } while (buscarTicketPorID(listaBaja, id) == NULL && buscarTicketPorID(listaMedia, id) == NULL && buscarTicketPorID(listaAlta, id) == NULL);
+                
+                scanf("%d", &id);
+                if (buscarTicketPorID(listaBaja, id) == NULL && buscarTicketPorID(listaMedia, id) == NULL && buscarTicketPorID(listaAlta, id) == NULL) {
+                    printf("El ID no existe. Ingrese otro ID: \n");
+                    break;
+                }
 
                 printf("Ingrese la nueva prioridad (1 para baja, 2 para media, 3 para alta): ");
                 do {
@@ -247,14 +286,16 @@ int main() {
                 system("cls||clear");
                 printf("Buscar ticket por ID:\n");
                 printf("Ingrese el ID del ticket: ");
-                do {
-                    scanf("%d", &id);
-                    if (buscarTicketPorID(listaBaja, id) == NULL && buscarTicketPorID(listaMedia, id) == NULL && buscarTicketPorID(listaAlta, id) == NULL) {
-                        printf("El ID no existe. Ingrese otro ID: ");
-                    }
-                } while (buscarTicketPorID(listaBaja, id) == NULL && buscarTicketPorID(listaMedia, id) == NULL && buscarTicketPorID(listaAlta, id) == NULL);
+                
+                scanf("%d", &id);
+                if (buscarTicketPorID(listaBaja, id) == NULL && buscarTicketPorID(listaMedia, id) == NULL && buscarTicketPorID(listaAlta, id) == NULL) {
+                    printf("El ID no existe. Ingrese otro ID: \n");
+                    break;
+                }
                 
                 buscarTicketPorIDyDetalles(listaBaja, id);
+                buscarTicketPorIDyDetalles(listaMedia, id);
+                buscarTicketPorIDyDetalles(listaAlta, id);
                 break;
             case 6:
                 printf("Saliendo del programa...\n");
@@ -272,5 +313,6 @@ int main() {
 
     return EXIT_SUCCESS;
 }
+
 
 
